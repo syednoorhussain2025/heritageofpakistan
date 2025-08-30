648e06b11b30072f09f095b9ce28463532e47e7b
Syed Noor <syed.noor.hussain88@gmail.com>
2025-08-30 10:43:11 +0500

    Your commit message

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,7 +4,7 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function createClient() {
+export async function createClient() {
   const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
