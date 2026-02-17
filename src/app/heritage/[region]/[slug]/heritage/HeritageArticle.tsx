d728eee4c2e9643d8b7061d0a808bb2a2c7ac856
Syed Noor <syed.noor.hussain88@gmail.com>
2026-02-17 13:29:42 +0500

    Your commit message

--- src/app/heritage/[region]/[slug]/heritage/HeritageArticle.tsx
+++ src/app/heritage/[region]/[slug]/heritage/HeritageArticle.tsx
@@ -710,6 +710,7 @@ export default function HeritageArticle({
         .reading-article .hop-text,
         .reading-article .flx-text {
           font-family: var(--font-lato), "Lato", sans-serif !important;
+          font-size: 0.96rem;
         }
         .reading-article [style*="font-family"] {
           font-family: var(--font-lato), "Lato", sans-serif !important;
