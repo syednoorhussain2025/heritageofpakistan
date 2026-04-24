86771625a7715adce7593353b363f91c8cb3e0e0
Syed Noor <syed.noor.hussain88@gmail.com>
2026-04-24 07:11:30 +0500

    fix(map): use NearbyMe map-pin SVG on location header button

--- src/app/map/MapClient.tsx
+++ src/app/map/MapClient.tsx
@@ -1828,7 +1828,9 @@ export default function MapClient() {
                 {gpsStatus === "granted" && gpsCoords && (
                   <span className="absolute inset-0 rounded-full bg-white/20 animate-ping" />
                 )}
-                <Icon name="location-arrow" size={20} className="text-white relative z-10" />
+                <svg className="w-5 h-5 text-white relative z-10" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                </svg>
               </>
             )}
           </span>
